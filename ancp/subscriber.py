27686d1160e070220839de67582ffe1f66c36058
Christian Giese <GIC-de@users.noreply.github.com>
2017-03-27 07:20:21 +0200

    add logging
    add logging

--- ancp/subscriber.py
+++ ancp/subscriber.py
@@ -6,6 +6,9 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from builtins import bytes
 import struct
+import logging
+
+log = logging.getLogger(__name__)
 
 # LINE STATE
 SHOWTIME = 1
